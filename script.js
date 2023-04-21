4805c62556e4e443bcaa6d747cd7fda5a90e5c9d
ANKIT KUMAR RAI <117643219+Ankit0315@users.noreply.github.com>
2023-04-21 13:36:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 //your JS code here. If required.
 function submit() {
+	var name= document.getElementbyId("username").value;
+	var password= document.getElementbyId("password").value;
 var check=document.getElementbyId("checkbox");
-	
-	
+
+	  if (document.getElementById('checkbox').checked) {
+            alert(`Logged in as ${name}`);
+        } else {
+            alert("You didn't check it! Let me check it for you.");
+        }
+    
+	console.log(name,password);
+	localStorage.setItem("name", name);
+	localStorage.setItem("password",password)
 }
